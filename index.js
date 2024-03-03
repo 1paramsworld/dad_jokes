233a12608fbcf37dec41fad7a1d6582da8ca2df5
Param Shah <param.sanjay.shah@gmail.com>
2024-03-03 20:30:39 +0530

    i hope it works

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const express=require("express");
-const app=express()
-const path=require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Use dynamic import to import node-fetch
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const port = process.env.PORT || 3000; // Use port from environment variable or default to 3000
+const port = process.env.PORT || 3000; 
 
-const uipath=path.join(__dirname)
-app.set("view engine","ejs")
-app.set("views",uipath);
+const uipath = path.join(__dirname);
+app.set("view engine", "ejs");
+app.set("views", uipath);
 
-app.get("/",(req,res)=>{
-    const url="https://icanhazdadjoke.com/slack";
-    fetch(url).then((data)=>{
-        return data.json()
-    }).then((data)=>{
-        res.render("ui",{hey1:data.attachments[0].fallback})
-    }).catch(()=>{
-        res.end("something is wrong")
-    })
-})
+app.get("/", (req, res) => {
+    const url = "https://icanhazdadjoke.com/slack";
+    fetch(url)
+        .then((data) => data.json())
+        .then((data) => {
+            res.render("ui", { hey1: data.attachments[0].fallback });
+        })
+        .catch(() => {
+            res.end("something is wrong");
+        });
+});
 
-app.post("/newquote",(req,res)=>{
-    res.redirect("/")
-})
+app.post("/newquote", (req, res) => {
+    res.redirect("/");
+});
 
-app.listen(port)
+app.listen(port);
