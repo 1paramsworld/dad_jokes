4df72006e643900a52e3543e08c492ac3a8a3e81
Param Shah <param.sanjay.shah@gmail.com>
2024-03-04 20:26:30 +0530

    hey param

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); 
 
 const express = require("express");
 const app = express();
 const path = require("path");
 
-// Use dynamic import to import node-fetch
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const port = process.env.PORT || 3000; 
@@ -14,15 +13,16 @@ app.set("view engine", "ejs");
 app.set("views", uipath);
 
 app.get("/", (req, res) => {
-    const url = "https://icanhazdadjoke.com/slack";
-    fetch(url)
-        .then((data) => data.json())
-        .then((data) => {
-            res.render("ui", { hey1: data.attachments[0].fallback });
-        })
-        .catch(() => {
-            res.end("something is wrong");
-        });
+    // const url = "https://icanhazdadjoke.com/slack";
+    // fetch(url)
+    //     .then((data) => data.json())
+    //     .then((data) => {
+    //         res.render("ui", { hey1: data.attachments[0].fallback });
+    //     })
+    //     .catch(() => {
+    //         res.end("something is wrong");
+    //     });
+    res.end("hey param")
 });
 
 app.post("/newquote", (req, res) => {
